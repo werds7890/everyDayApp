7c6e9de692decf96c32c0f8baf608a95d90e715c
{gusdk78951} <{gusdk78951@gmail.com}>
2023-08-15 20:49:54 +0900

    UI 작업 완료

--- app/src/main/java/com/example/everydayapp/TeamActivity.kt
+++ app/src/main/java/com/example/everydayapp/TeamActivity.kt
@@ -3,9 +3,9 @@ package com.example.everydayapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TeamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_team)
     }
 }
