e09fdceb435afbdd9f4ede7d9fdf1d70abc24e7a
{gusdk78951} <{gusdk78951@gmail.com}>
2023-08-16 09:27:18 +0900

    activity 수정

--- app/src/main/java/com/example/everydayapp/TeamActivity.kt
+++ app/src/main/java/com/example/everydayapp/TeamActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class TeamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_team)
+        setContentView(R.layout.activity_main)
     }
 }
