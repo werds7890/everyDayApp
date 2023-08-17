3d907ef85d9fc1cbd6773cc28cb45e3435bac678
somethinginthebox <ha862888@gmail.com>
2023-08-17 17:49:29 +0900

    [FEAT] : ⚙️ intent, textWatcher 사용 & textWatcher에 따른 토스트 사용

--- app/src/main/java/com/example/everydayapp/LoginPage.kt
+++ app/src/main/java/com/example/everydayapp/LoginPage.kt
@@ -1,21 +1,38 @@
 package com.example.everydayapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginPage : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
-       val loginBtn = findViewById<Button>(R.id.btn_login)
+        val loginBtn = findViewById<Button>(R.id.btn_login)
         val signUpBtn = findViewById<Button>(R.id.btn_signUp)
         val loginIdEditText = findViewById<EditText>(R.id.editId)
         val loginPwEditText = findViewById<EditText>(R.id.editPw)
+
+        //Id data 받기
+        val idData = intent.getStringExtra("dataFromSignUpPage")
+        loginIdEditText.setText(idData)
+        val idDataToIntent = Intent(this, MainActivity::class.java)
+        idDataToIntent.putExtra("dataFromSignUpPage",idData)
+        startActivity(idDataToIntent)
+
+        //teamName data 받기
+        val teamNameData = intent.getStringExtra("TeamNamedataFromSignUpPage")
+        //
+        val teamNameToIntent = Intent(this, MainActivity::class.java)
+        teamNameToIntent.putExtra("dataFromSignUpPage",teamNameData)
+        startActivity(teamNameToIntent)
+
 
         loginBtn.setOnClickListener {
             val loginId = loginIdEditText.text.toString()
@@ -28,13 +45,16 @@ class LoginPage : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show()
 
-                val loginIntent = Intent(this, SingleUpPage::class.java)
-                intent.putExtra("dataFromSingleInActivity",loginId)
-                startActivity(loginIntent)
+                //intent 메인으로 넘어가기 추가.
+                val intoMainIntent = Intent(this, MainActivity::class.java)
+                startActivity(intoMainIntent)
+
             }
         }
+
+        //회원가입페이지로 이동.
         signUpBtn.setOnClickListener {
-            val signUpIntent = Intent(this, SingleUpPage::class.java)
+            val signUpIntent = Intent(this, SignUpPage::class.java)
             startActivity(signUpIntent)
         }
     }
