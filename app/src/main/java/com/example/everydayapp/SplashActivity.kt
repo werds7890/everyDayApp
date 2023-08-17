5756fc625ab64e931af94733e96020858d45bd4c
{gusdk78951} <{gusdk78951@gmail.com}>
2023-08-17 10:40:03 +0900

    Splash 작업 완료

---
+++ app/src/main/java/com/example/everydayapp/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.everydayapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(applicationContext, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
