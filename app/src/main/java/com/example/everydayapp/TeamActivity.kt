3a802dfe75cc1ef5ecbb5764141eccf627d1ebdd
{gusdk78951} <{gusdk78951@gmail.com}>
2023-08-17 20:58:02 +0900

    TeamPage 텍스트 수정/저장 기능 추가

--- app/src/main/java/com/example/everydayapp/TeamActivity.kt
+++ app/src/main/java/com/example/everydayapp/TeamActivity.kt
@@ -2,10 +2,32 @@ package com.example.everydayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 
 class TeamActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
+
+        val editBtn = findViewById<ImageButton>(R.id.editBtn)
+        val saveBtn = findViewById<ImageButton>(R.id.saveBtn)
+        val editText = findViewById<EditText>(R.id.editText)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.visibility
+
+        editBtn.setOnClickListener {
+                textView.visibility = View.GONE
+                editText.visibility = View.VISIBLE
+                editText.setText(textView.text)
+        }
+        saveBtn.setOnClickListener {
+                textView.visibility = View.VISIBLE
+                editText.visibility = View.GONE
+                textView.text = editText.text
+        }
     }
 }
