05b0b2c6b23cfdd6dd2c420318d26b2fb79316aa
monsterKyu <ilkyu092@naver.com>
2023-08-17 12:37:23 +0900

    스크롤 완료

--- app/src/main/java/com/example/everydayapp/Detail.kt
+++ app/src/main/java/com/example/everydayapp/Detail.kt
@@ -2,10 +2,14 @@ package com.example.everydayapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.os.persistableBundleOf
 
 class Detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
     }
+
 }
