4766e699359efcc94111c5ff68cc2a29abcb6015
{gusdk78951} <{gusdk78951@gmail.com}>
2023-08-19 14:38:11 +0900

    갤러리 연동/ 이미지 변경

--- app/src/main/java/com/example/everydayapp/TeamActivity.kt
+++ app/src/main/java/com/example/everydayapp/TeamActivity.kt
@@ -1,19 +1,30 @@
 package com.example.everydayapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import org.w3c.dom.Text
 
 class TeamActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private fun loadImage(uri: Uri) {
+        Glide.with(this)
+            .load(uri)
+            .into(imageView)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
 
+        // 아이디, 팀명, 좌우명 가져오기
         val myPgTeamName=findViewById<TextView>(R.id.myPageTeamName)
         val myPgId=findViewById<TextView>(R.id.myPageId)
         val myPgMotto=findViewById<TextView>(R.id.myPageMotto)
@@ -24,6 +35,20 @@ class TeamActivity : AppCompatActivity() {
         myPgMotto.setText(intent.getStringExtra("motto"))
         myPgTeamName2.setText(intent.getStringExtra("teamNameData"))
 
+        // 버튼 클릭 시 갤러리 연동, 이미지 불러오기
+        val imageBtn = findViewById<ImageButton>(R.id.imageBtn)
+        imageView = findViewById(R.id.imageView)
+
+        val getImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                loadImage(uri)
+            }
+        }
+        imageBtn.setOnClickListener {
+            getImage.launch("image/*")
+        }
+
+        // 버튼 클릭 시 텍스트 수정,저장
         val editBtn = findViewById<ImageButton>(R.id.editBtn)
         val saveBtn = findViewById<ImageButton>(R.id.saveBtn)
         val editText = findViewById<EditText>(R.id.editText)
